f365c3f4cbac42a2ad582a518a3a99ee31135855
KoseiImai <koseiimai2021@gmail.com>
2021-07-15 23:04:10 +0900

    helloComponentの修正

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hello.component.css']
 })
 export class HelloComponent implements OnInit {
+  // プロパティ・・・クラスに値を保管しておくのに使用。
   title: string;
   message: string;
 
