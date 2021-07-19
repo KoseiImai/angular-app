3d22ee7fe9496ecb41c1c32c0e6ba4cf5146af4a
KoseiImai <koseiimai2021@gmail.com>
2021-07-19 23:24:51 +0900

    インターポレーションについて1

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -9,12 +9,22 @@ export class HelloComponent implements OnInit {
   // プロパティ・・・クラスに値を保管しておくのに使用。
   title: string;
   message: string;
+  price: number;
+  now: Date;
 
-  constructor() { }
+  constructor() { 
+    setInterval(
+      () => { this.now = new Date();}, 1000
+    );
+  }
 
   ngOnInit(): void {
     this.title = 'Hello app';
     this.message = 'This is My First Component!!';
+    this.price = 123450;
   }
 
+  today () {
+    return new Date().toLocaleString();
+  }
 }
